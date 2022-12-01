7a810bfe2b285824b3048139394c21f9ff8f30f4
Utso Sarkar <87900549+Utso-Sarkar@users.noreply.github.com>
2022-12-02 00:01:28 +0600

    Add files via upload
    simple interview question Js

---
+++ swap.js
@@ -0,0 +1,16 @@
+var a =5;
+var b =6;
+console.log("Value of a :",a , "Value of b :",b)
+var temp=a;
+a=b;
+b=temp;
+console.log("Value of a :",a , "Value of b :",b)
+
+
+
+
+p=5;
+q=6;
+[p,q]=[q,p]
+console.log("Value of p :",p , "Value of q :",q)
+
